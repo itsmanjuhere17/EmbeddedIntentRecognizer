cdba0db805f42790b428a52eb370b9ca3712f41b
manju malepati <manjumalepati17@gmail.com>
2020-08-15 22:24:29 +0300

    Modified accepting input as command line to user interactive manner.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include<memory>
+#include<regex>
 using namespace std;
 
 #include "constants.h"
 #include "IfIntentProvider.h"
 #include "intentproviderfactory.h"
-int main(int argc, char** argv)
+int main()
 {
-    if(argc < 2) {
-        std::cerr<<"Please input a text."<<std::endl;
-        std::exit(1);
+    std::cout<<std::endl<<"############# EMBEDDED INTENT RECOGNIZER ###############"<<std::endl<<std::endl;
+    std::cout<<"Please enter a input text. If you want to exit, say good bye!"<<std::endl;
+    while(1) {
+        std::string input_text;
+        std::getline(std::cin, input_text);
+        //Convert input to lower_case.
+        std::transform(input_text.begin(), input_text.end(), input_text.begin(), [](char ch){
+            return std::tolower(ch);
+        });
+        if(regex_match(input_text, std::regex("(good)?(.*)(bye)+(.*)"))) {
+            std::cout<<"Good Bye!!"<<std::endl;
+            break;
+        }
+        auto intent_prov = IntentProviderFactory::getIntentProvider(Constants::IntentProviders::Version::BASIC);
+        if(intent_prov) {
+            intent_prov->parseInput(input_text);
+            std::cout<<"Intent:"<<intent_prov->getIntent()<<std::endl<<std::endl;
+        }
     }
-    //Read command line input.
-    std::string input_text;
-    for(auto index=1 ;index < argc; ++index) {
-        input_text += argv[index];
-        input_text += " ";
-    }
-    auto intent_prov = IntentProviderFactory::getIntentProvider(Constants::IntentProviders::Version::BASIC);
-    if(intent_prov) {
-        intent_prov->parseInput(input_text);
-        std::cout<<intent_prov->getIntent()<<std::endl;
-    }
+
     return 0;
 }
